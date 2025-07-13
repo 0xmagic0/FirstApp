8b3b5b3b8061ecdaab250e7a8f24f9483bfa3282
Francisco Rosales <102229946+0xmagic0@users.noreply.github.com>
2025-07-13 12:15:59 -0500

    http request

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -25,7 +25,6 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
